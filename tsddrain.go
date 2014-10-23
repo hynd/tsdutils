3efd23de9f9d24a0652b4bf1718457a6ae1cfc58
Kieren Hynd <kieren.hynd@ticketmaster.co.uk>
2014-10-23 09:19:10 +0100

    Added stats and output options
    * Ability to drop the messages (ie; for load testing) when no output
      is set (the default), or write to stdout with "-o -"
    * Added some stats tracking - sending a "stats" command to the tcp
      port prints the stats, "reset" clears them.
    * Switched most stdout output to use "log" so timestamps are printed.

--- tsddrain.go
+++ tsddrain.go
@@ -1,96 +1,133 @@
 package main
 
 import (
-    "bufio"
-    "net"
-    "fmt"
-    "os"
-    "flag"
-    "strings"
-    "time"
+	"bufio"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"strings"
+	"time"
 )
 
 const (
-  versionString = "tsddrain v1.0\n"
+	versionString = "tsddrain v1.0\n"
 )
 
 var (
-  tHost         = flag.String("b", "localhost:4242", "bind host")
-  outFile       = flag.String("o", "/tmp/tsd.out", "output file")
-  flushInterval = flag.Duration("f", time.Second * 2, "flush interval")
+	tHost         = flag.String("b", "localhost:4242", "bind host")
+	outFile       = flag.String("o", "", "output file")
+	flushInterval = flag.Duration("f", time.Second*2, "flush interval")
 )
+
+type ServerStat struct {
+	events  int64
+	bytes   int64
+	unknown int64
+	conns   int
+	open    int
+}
 
 func main() {
 
-  flag.Parse()
-  msgChan := make(chan string, 16)
+	flag.Parse()
+	stats := ServerStat{}
 
-  go func() {
-    f, err := os.OpenFile(*outFile, os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0660)
-    if err != nil {
-      fmt.Println("Error opening file", *outFile, err)
-      os.Exit(1)
-    }
-    defer f.Close()
+	msgChan := make(chan string, 16)
+	if *outFile != "" && *outFile != "-" {
+		go writeFile(msgChan)
+	}
 
-    w := bufio.NewWriter(f)
-    ticker := time.Tick(*flushInterval)
+	s, err := net.Listen("tcp", *tHost)
+	if err != nil {
+		log.Fatalln("Couldn't start server", err)
+	}
 
-    for {
-      select {
-        case event := <-msgChan:
-          _, err = fmt.Fprintln(w, event)
-          if err != nil {
-            fmt.Println("Error writing to file", err)
-          }
-        case <-ticker:
-          w.Flush()
-      }
-    }
-  }()
+	for {
+		conn, err := s.Accept()
+		if err != nil {
+			log.Println("Failed to accept connection", err)
+			continue
+		}
+		log.Println("Accepted new connection from", conn.RemoteAddr().String())
 
+		go func() {
+			defer func() {
+				stats.open -= 1
+				conn.Close()
+			}()
+			stats.conns++
+			stats.open++
 
-  s, err := net.Listen("tcp", *tHost)
-  if err != nil {
-    fmt.Println("Couldn't start server", err)
-    os.Exit(1)
-  }
+			scanner := bufio.NewScanner(conn)
+			writer := bufio.NewWriter(conn)
 
-  for {
-    conn, err := s.Accept()
-    if err != nil {
-      fmt.Println("Failed to accept connection", err)
-      continue
-    }
-    fmt.Println("Accepted new connection from", conn.RemoteAddr().String())
+			for scanner.Scan() {
+				// extending the deadline on every loop pass seems to be idiomatic, but expensive
+				//conn.SetReadDeadline(time.Now().Add(time.Duration(6000 * time.Millisecond)))
+				event := scanner.Text()
+				switch {
+				case strings.HasPrefix(event, "put "):
+					stats.events++
+					stats.bytes += int64(len(event))
+					if *outFile != "" {
+						if *outFile == "-" {
+							log.Println(event)
+						} else {
+							msgChan <- event
+						}
+					}
+				case strings.HasPrefix(event, "version"):
+					writer.WriteString(versionString)
+					writer.Flush()
+				case strings.HasPrefix(event, "stats"):
+					t := time.Now().Unix()
+					fmt.Fprintf(writer, "tsdsink.events %d %d\n", t, stats.events)
+					fmt.Fprintf(writer, "tsdsink.bytes %d %d\n", t, stats.bytes)
+					fmt.Fprintf(writer, "tsdsink.connections %d %d\n", t, stats.conns)
+					fmt.Fprintf(writer, "tsdsink.open %d %d\n", t, stats.open)
+					fmt.Fprintf(writer, "tsdsink.unknown %d %d\n", t, stats.unknown)
+					writer.Flush()
+				case strings.HasPrefix(event, "reset"):
+					log.Println("Resetting stats")
+					stats = ServerStat{}
+				default:
+					log.Println("Unknown command", event)
+					stats.unknown++
+				}
+			}
+			if err := scanner.Err(); err != nil {
+				log.Println("Error reading standard input", err)
+			}
 
-    go func() {
-      defer conn.Close()
+		}()
+	}
 
-      scanner := bufio.NewScanner(conn)
-      writer  := bufio.NewWriter(conn)
+	log.Println("Listening on %s for connections", tHost)
 
-      for scanner.Scan() {
-        event := scanner.Text()
-        switch {
-          case strings.HasPrefix(event, "put "):
-            msgChan<- event
-          case strings.HasPrefix(event, "version"):
-            writer.WriteString(versionString)
-            writer.Flush()
-          default:
-            fmt.Println("Unknown command", event)
-        }
-      }
-      if err := scanner.Err(); err != nil {
-        fmt.Println("Error reading standard input", err)
-      }
-
-    }()
-  }
-
-  fmt.Println("Listening on %s for connections", tHost)
-
-  select{}
+	select {}
 }
 
+func writeFile(msgChan chan string) {
+	f, err := os.OpenFile(*outFile, os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0660)
+	if err != nil {
+		log.Fatalln("Error opening file:", err)
+	}
+	defer f.Close()
+
+	w := bufio.NewWriter(f)
+	ticker := time.Tick(*flushInterval)
+
+	for {
+		select {
+		case event := <-msgChan:
+			_, err = fmt.Fprintln(w, event)
+			if err != nil {
+				log.Println("Error writing to file", err)
+			}
+		case <-ticker:
+			w.Flush()
+		}
+	}
+}
